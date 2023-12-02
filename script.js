943662e4c5619d262f288265c88d131be3785ec9
ayush0355 <ayushsharma3081@gmail.com>
2023-12-02 21:12:31 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,12 @@
+const countValue=document.querySelector('.maindiv');
+
+const increament = ()=>{
+    let value=parseInt(countValue.innerText);
+    value=value+1;
+    countValue.innerText=value;
+}
+const decreament= () =>{
+    let value=parseInt(countValue.innerText);
+    value=value-1;
+    countValue.innerText=value;
+}
